042b5ee80f9d231d50ca2c336a99f059b77a1a47
Amr A.Mohammed <merodiro@gmail.com>
2016-08-21 09:11:34 +0200

    make images show well on phone

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 // app/adapters/application.js
 export default DS.JSONAPIAdapter.extend({
-  host: 'https://salezaty-api.herokuapp.com'
+  host: 'http://localhost:3000'
 });
